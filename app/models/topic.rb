b4a270b40c5c7d6ea74b1ab3fc5c029ad8cdbe6a
Toba <marxdm02@gmail.com>
2020-02-13 07:33:57 +0000

    fourth commit

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,10 @@ class Topic < ApplicationRecord
   validates :description, presence: true
   validates :image, presence: true
   
-  belongs_to :user_id
+  belongs_to :user
   
   mount_uploader :image, ImageUploader
+  
+  has_many :favorites
+  has_many :favorite_users, through: :favorites, source: 'user'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'topics/new'
   get 'sessions/new'
-  get 'users/new'
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'pages#index'
   get 'pages/help'
-  
-  resources :users
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
@@ -15,4 +10,9 @@ Rails.application.routes.draw do
   
   resources :users
   resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to:'favorites#destroy'
+  
 end
